258f3a0d197997b3f70f16f8b648e5c8584e180a
Michael Scharhag <m.scharhag@gmail.com>
2014-06-27 19:22:05 +0200

    Added sample project for markdown usage

---
+++ markdown/src/main/java/com/mscharhag/Test.java
@@ -0,0 +1,48 @@
+package com.mscharhag;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+	/**
+	 * ## Large headline
+	 * ### Smaller headline
+	 *
+	 * This is a comment that contains `code` parts.
+	 *
+	 * Code blocks:
+	 *
+	 * ```java
+	 * int foo = 42;
+	 * System.out.println(foo);
+	 * ```
+	 *
+	 * Quote blocks:
+	 *
+	 * > This is a block quote
+	 *
+	 * lists:
+	 *
+	 *  - first item
+	 *  - second item
+	 *  - third item
+	 *
+	 * This is a text that contains an [external link][link].
+	 *
+	 * [link]: http://external-link.com/
+	 *
+	 * @param id the user id
+	 * @return the user object with the passed `id` or `null` if no user with this `id` is found
+	 */
+	public User finAdUser(long id) {
+		return null;
+	}
+
+
+	static class User {
+
+	}
+}
